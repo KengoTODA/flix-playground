acf33f97fb94577c3e6680d280b66f59434f122e
Kengo TODA <skypencil@gmail.com>
2021-12-14 20:28:09 +0800

    remove needless config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     `java-library`
     id("jp.skypencil.flix")
 }
-
-tasks.named<Jar>("jar") {
-    from("$buildDir/classes/flix/main")
-}
-
-tasks.named<Task>("classes") {
-    dependsOn("compileFlix")
-}
