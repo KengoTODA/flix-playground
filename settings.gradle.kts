7c8d81b988ad70bb20d8d0838e53064be76c9eab
Kengo TODA <skypencil+github@gmail.com>
2022-01-06 13:13:54 +0800

    build: use the released Flix plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,1 @@
-// https://docs.gradle.org/7.3/userguide/composite_builds.html#included_plugin_builds
-pluginManagement {
-    includeBuild("../flix-gradle-plugin")
-}
 rootProject.name = "flix-playground"
